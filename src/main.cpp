ddc39aaedd55a8778fe265caa6f1c6976c387428
Guy Thomas <guy@grtyvr.com>
2022-01-23 23:43:58 -0800

    updated abstract code to match blog post

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 class Component {
 	public:
+		Component(){
+		}
 		virtual ~Component() {}
-		virtual void method() = 0;
+		virtual std::string method() const = 0;
 		// etc...
 };
 
 class ConcreteComponent : public Component {
 	public:
+		ConcreteComponent () {
+		}
 		virtual ~ConcreteComponent() {}
-		void method() {
-			std::cout << "method" << std::endl;
+		std::string method() const override {
+			return "concrete component";
 		}
 		// etc...
 };
@@ -20,46 +24,63 @@ class Decorator : public Component {
 		// a reference to the Component that we will be decorating
 		Component *component_;
 	public:
-		// the interface conforms to the Component interface
-		~Decorator() {}
+		// the component we are decorating will manage destruction
+		// store a reference to our decorated Component type.
 		Decorator(Component *component) : component_(component) {}
-		virtual void method() {
-			component_->method();
+		// the interface conforms to the Component interface
+		// we can call the components method if we like.
+		std::string method() const override {
+			return component_->method();
 		}
 };
 
 class ConcreteDecoratorA : public Decorator {
 	public:
 		ConcreteDecoratorA(Component *component) : Decorator(component) {}
-		void method() {
-			Decorator::method();
+		std::string method() const override{
 			// add capabilities for this decorator
-			std::cout << "Decorator A" << std::endl;
+			// note that the Decorator::method() will resolve to the method of
+			// the component object that was used to initialize this concrete
+			// decorator.
+			return "Decorator A (" + Decorator::method() + ")";
 		}
 };
 
 class ConcreteDecoratorB : public Decorator {
 	public:
 		ConcreteDecoratorB( Component *component) : Decorator(component) {}
-		void method() {
-			Decorator::method();
+		std::string method() const override{
 			// add capabilities for this decorator.
-			std::cout << "Decorator B" << std::endl;
+			// note that the Decorator::method() will resolve to the method of
+			// the component object that was used to initialize this concrete
+			// decorator.
+			return "Decorator B (" + Decorator::method() + ")";
 		}
 };
 
 int main(int argc, char *argv[])
 {
+	// create a concrete component that implements the interface of Component
 	ConcreteComponent *concreteComponent = new ConcreteComponent();
+	// create decorated versions
+	// first decorate with ConcreteDecoratorA
 	ConcreteDecoratorA *decoratorA = new ConcreteDecoratorA(concreteComponent);
+	// take the decorated resulting object and pass that 
+	// as a paramater to the ConcreteDecoratorB
 	ConcreteDecoratorB *decoratorB = new ConcreteDecoratorB(decoratorA);
+	// decorate the original ConcreteComponent with ConcreteDecoratorB
+	ConcreteDecoratorB *decoratorC = new ConcreteDecoratorB(concreteComponent);
 
 	Component *component = decoratorB;
-	component->method();
-	decoratorA->method();
-	concreteComponent->method();
+
+	std::cout << component->method() << std::endl;
+	std::cout << decoratorA->method() << std::endl;
+	std::cout << concreteComponent->method() << std::endl;
+	std::cout << decoratorB->method() << std::endl;
+	std::cout << decoratorC->method() << std::endl;
 
 	delete decoratorA;
 	delete decoratorB;
+	delete decoratorC;
 	delete concreteComponent;
 }
